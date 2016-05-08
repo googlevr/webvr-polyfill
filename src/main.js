ad012382ae69215dc3f942e649e6157e80007bff
Christopher Van <cvan@mozilla.com>
2016-05-07 20:09:56 -0700

    rename (and deprecate) WebVRConfig options for consistency (fixes #100)

--- src/main.js
+++ src/main.js
@@ -15,10 +15,51 @@
 var Util = require('./util.js');
 var WebVRPolyfill = require('./webvr-polyfill.js');
 
+var RENAMED_BOOL_CONFIG_OPTIONS = {
+  FORCE_ENABLE_VR: {
+    new_key: 'ENABLE_VR',
+    inverse: false
+  },
+  TOUCH_PANNER_DISABLED: {
+    new_key: 'TOUCH_PANNER',
+    inverse: true
+  },
+  MOUSE_KEYBOARD_CONTROLS_DISABLED: {
+    new_key: 'MOUSE_KEYBOARD_CONTROLS',
+    inverse: true
+  },
+  ENABLE_DEPRECATED_API: {
+    new_key: 'USE_DEPRECATED_API',
+    inverse: false
+  }
+};
+
+// To be nice, we tell the developers of the renamed config options —
+// and until deprecation is announced and known — we gracefully change the
+// values for them if they are using the deprecated keys.
+if (window.WebVRConfig) {
+  var option;
+  Object.keys(RENAMED_BOOL_CONFIG_OPTIONS).forEach(function (oldKey) {
+    option = RENAMED_BOOL_CONFIG_OPTIONS[oldKey];
+    if (oldKey in window.WebVRConfig) {
+      // If necessary, inverse value of config option boolean.
+      window.WebVRConfig[option.new_key] = option.inverse ? !window.WebVRConfig[oldKey] : window.WebVRConfig[oldKey];
+      delete window.WebVRConfig[oldKey];
+      console.warn(
+        '`window.WebVRConfig.%s` is deprecated and will be removed soon. ' +
+        'Please change to `window.WebVRConfig.%s = %s`.',
+        oldKey,
+        option.new_key,
+        window.WebVRConfig[option.new_key]
+      );
+    }
+  });
+}
+
 // Initialize a WebVRConfig just in case.
 window.WebVRConfig = Util.extend({
-  // Forces availability of VR mode, even for non-mobile devices.
-  FORCE_ENABLE_VR: false,
+  // Force availability of VR mode, even for non-mobile devices.
+  ENABLE_VR: false,
 
   // Complementary filter coefficient. 0 for accelerometer, 1 for gyro.
   K_FILTER: 0.98,
@@ -26,23 +67,24 @@ window.WebVRConfig = Util.extend({
   // How far into the future to predict during fast motion (in seconds).
   PREDICTION_TIME_S: 0.040,
 
-  // Flag to disable touch panner. In case you have your own touch controls.
-  TOUCH_PANNER_DISABLED: false,
+  // Flag to enable touch panner. You may want to disable if you have your
+  // own touch controls.
+  TOUCH_PANNER: true,
 
   // Enable yaw panning only, disabling roll and pitch. This can be useful
   // for panoramas with nothing interesting above or below.
   YAW_ONLY: false,
 
-  // To disable keyboard and mouse controls, if you want to use your own
+  // To toggle keyboard and mouse controls, if you want to use your own
   // implementation.
-  MOUSE_KEYBOARD_CONTROLS_DISABLED: false,
+  MOUSE_KEYBOARD_CONTROLS: true,
 
   // Prevent the polyfill from initializing immediately. Requires the app
   // to call InitializeWebVRPolyfill() before it can be used.
   DEFER_INITIALIZATION: false,
 
   // Enable the deprecated version of the API (navigator.getVRDevices).
-  ENABLE_DEPRECATED_API: false,
+  USE_DEPRECATED_API: false,
 
   // Scales the recommended buffer size reported by WebVR, which can improve
   // performance.
